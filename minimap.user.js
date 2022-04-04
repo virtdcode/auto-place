59ea401dc34d9c658be7e22a27a2aa91c192012c
virtdcode <pablo98oses@gmail.com>
2022-04-04 22:00:21 +0200

    fix message

--- minimap.user.js
+++ minimap.user.js
@@ -6,8 +6,8 @@
 // @version     0.4
 // @grant       GM.xmlHttpRequest
 // @author      Ponywka, bb010g
-// @downloadURL https://raw.githubusercontent.com/r-ainbowroad/minimap/d/main/minimap.user.js
-// @updateURL   https://raw.githubusercontent.com/r-ainbowroad/minimap/d/main/minimap.user.js
+// @downloadURL https://raw.githubusercontent.com/virtdcode/auto-place/raw/main/minimap.user.js
+// @updateURL   https://raw.githubusercontent.com/virtdcode/auto-place/raw/main/minimap.user.js
 // @connect     raw.githubusercontent.com
 // @connect     media.githubusercontent.com
 // @require     https://unpkg.com/uhtml@2.8.1
