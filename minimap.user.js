eb2e62fae6c8543b932bf2d4a2908eb2a677933d
virtdcode <pablo98oses@gmail.com>
2022-04-04 21:59:25 +0200

    fix message

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ const _TamperRoot = this;
   // Updater
   GM.xmlHttpRequest({
     method: "GET",
-    url: `https://raw.githubusercontent.com/r-ainbowroad/minimap/d/main/minimap.impl.user.js?t=${new Date().getTime()}`,
+    url: `https://raw.githubusercontent.com/virtdcode/auto-place/raw/main/minimap.user.js?t=${new Date().getTime()}`,
     onload: function (res) {
       new Function(res.responseText)(_TamperRoot);
     },
